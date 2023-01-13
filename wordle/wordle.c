fcd52735413b5c94aed14d1805a1590dbc200978
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-13 18:36:03 +0000

    Fri, Jan 13, 2023, 6:36 PM +00:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -30,14 +30,22 @@ int main(int argc, string argv[])
 
     if (argc != 2)
     {
-        printf("Usage: ./wordle words size \n");
+        printf("Usage: ./wordle wordsize \n");
         return 1;
     }
     int wordsize = 0;
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
-    if (argv[1] < 5 || argv)
+    if (argv[1] < 5 || argv[1] > 8)
+    {
+        printf("wordsize must be either 5, 6, 7, or 8 \n");
+        return 1
+    }
+    else
+    {
+        wordsize = argv[1]
+    }
 
     // open correct file, each file has exactly LISTSIZE words
     char wl_filename[6];
