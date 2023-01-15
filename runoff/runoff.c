ec365205361e5cc1ec92c1e782fe181459337233
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-15 11:57:33 +0000

    Sun, Jan 15, 2023, 11:57 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -129,6 +129,13 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
+    for(int i = 0 ;i< candidate_count ; i++)
+    {
+        if(strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i]
+        }
+    }
     return false;
 }
 
