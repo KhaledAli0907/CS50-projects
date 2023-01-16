83e375f5ab6bd5e8b6a9a14918a62189de92edee
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-16 12:05:25 +0000

    Mon, Jan 16, 2023, 12:05 PM +00:00

--- runoff/try.c
+++ runoff/try.c
@@ -1,3 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+
+int candidates_votes=5;
+int candidates_count=10;
+
+int main(void)
+{
+    double percent = candidates_votes / candidates_count;
+    printf("%f \n",percent);
+}
