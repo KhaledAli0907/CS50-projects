39824a89434c7fd31cc0487fe8dee35bf34486ab
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-16 12:27:25 +0000

    Mon, Jan 16, 2023, 12:27 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -158,10 +158,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    
+
     for(int i = 0; i<candidate_count; i++)
     {
-        float percentage = (float) candidates[i].votes / candidates_count;
+        float percentage = (float) candidates[i].votes / candidate_count;
         if( percentage >.5)
         {
             printf("%s is the winner",candidates[i].name);
