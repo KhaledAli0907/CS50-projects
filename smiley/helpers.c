50c4da8ac636cbb9fd23bccc6a619391a58fc058
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-19 20:57:56 +0000

    Thu, Jan 19, 2023, 8:57 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,7 +7,9 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
-            if(image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue )
+            //checks if all pixels are black
+            if(image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue)
+
         }
     }
 }
