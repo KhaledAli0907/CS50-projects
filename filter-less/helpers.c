952e4da582b0c9194c0b3b661c369cc3a51b86ac
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-20 10:37:40 +0000

    Fri, Jan 20, 2023, 10:37 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -30,14 +30,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-                feeding the sepia filter equations and rounding
+                //feeding the sepia filter equations and rounding
                 float SepiaRedf = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
                 int SepiaRed = round(SepiaRedf);
                 if(SepiaRed > MaxValue) //making sure that the values between 0 and 255
                 {
                     SepiaRed = MaxValue;
+                    image[i][j].rgbtRed = SepiaRed;
                 }
-                else{return;}
+                else
+                {
+                    image[i][j].rgbtRed = SepiaRed
+                }
                 float SepiaGreenf = .349 * image[i][j].rgbtRed + .686 *  image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
                 int SepiaGreen = round(SepiaGreenf);
                 if(SepiaGreen > MaxValue)
