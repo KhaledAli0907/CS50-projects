ea3ac9cd487a1fb2962eaa0c19a1072f32444fb9
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-20 09:48:43 +0000

    Fri, Jan 20, 2023, 9:48 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -2,6 +2,9 @@
 #include <math.h>
 
 int MaxInteger = 255;
+int originalRed = image[i][j].rgbtRed;
+int originalBlue = image[i][j].rgbtBlue;
+int originalGreen = image[i][j].rgbtGreen;
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -10,13 +13,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             //calculate the avreage RGB values
-            float AverageRGB = ((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            float AverageRGB = ((originalRed + originalGreen + originalBlue) / 3);
             //Round the Average to the nearest integer
             int Average = round(AverageRGB);
             //Set the RGB values to the Average to create the filter
-            image[i][j].rgbtRed = Average;
-            image[i][j].rgbtGreen = Average;
-            image[i][j].rgbtBlue = Average;
+            originalRed = Average;
+            originalGreen = Average;
+            originalBlue = Average;
         }
     }
     return;
@@ -25,8 +28,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //feeding the sepia filter equation
+            int SepiaFilter = ()
+        }
     return;
+    }
 }
 
 // Reflect image horizontally
