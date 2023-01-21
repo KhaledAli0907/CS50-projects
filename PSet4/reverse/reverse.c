c86fc19bf315d85ce08969c8b0ebf57bd7b0c20b
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-21 21:55:00 +0000

    Sat, Jan 21, 2023, 9:55 PM +00:00

--- PSet4/reverse/reverse.c
+++ PSet4/reverse/reverse.c
@@ -11,7 +11,10 @@ int main(int argc, char *argv[])
 {
     // Ensure proper usage
     // TODO #1
-
+    if (argc != 3)
+    {
+        printf("Usa")
+    }
     // Open input file for reading
     // TODO #2
 
