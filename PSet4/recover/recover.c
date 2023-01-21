4a4316c55aa9cd65e6de6a3719c859c0808e165f
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-21 19:57:24 +0000

    Sat, Jan 21, 2023, 7:57 PM +00:00

--- PSet4/recover/recover.c
+++ PSet4/recover/recover.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    //open memory card
+    //repeat untill end of the card
+        //read 512 bytes intobuffer
+        //if start of new JPEG
+            //if first JPEG
+                //start writing new
+            //else
+                //keep writing
+        //else
+            //if alreadyfound JPEG
 }
