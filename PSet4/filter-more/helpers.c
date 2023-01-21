489e779baa2a6fa45ef9d5b19ad6a6e93efb74dd
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-21 15:10:11 +0000

    Sat, Jan 21, 2023, 3:10 PM +00:00

--- PSet4/filter-more/helpers.c
+++ PSet4/filter-more/helpers.c
@@ -1,17 +1,33 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    //calculate the avrage value for each color
-    for ()
-
+    //create a nested loob to go through every pixel
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //calculate the average RGB values
+            int Average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.00);
+            //pass the Average value to the Pixels
+            image[i][j].rgbtRed = Average;
+            image[i][j].rgbtGreen = Average;
+            image[i][j].rgbtBlue = Average;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    
+
+
+
+
     return;
 }
 
