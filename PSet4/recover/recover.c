1dd0708286ba9dc255a50d86faeb5bb375f0fb19
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-21 20:08:25 +0000

    Sat, Jan 21, 2023, 8:08 PM +00:00

--- PSet4/recover/recover.c
+++ PSet4/recover/recover.c
@@ -3,10 +3,20 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        printf("Usage : ./recover image \n");
+        return 1;
+    }
     //open memory card
-    FILE *MemoryCard = fopen(card, "r");
+    FILE *MemoryCard = fopen(argv[1], "r");
+    if (MemoryCard == NULL)
+    {
+        printf("Error while opining the card \n")
+        return 1;
+    }
     //repeat untill end of the card
-    
+
         //read 512 bytes intobuffer
         //if start of new JPEG
             //if first JPEG
