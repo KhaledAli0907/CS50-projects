011758cc6dbddd07ce69e59732db2aa229f7cc73
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-22 20:42:48 +0000

    Sun, Jan 22, 2023, 8:42 PM +00:00

--- PSet4/recover/recover.c
+++ PSet4/recover/recover.c
@@ -9,9 +9,19 @@ int main(int argc, char *argv[])
     //declare varuables
 
     //check if the input argument is valid
-
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE")
+        return 1;
+    }
     //Open MemoryCard
-
+    FILE *MemoryCard = fopen (argv[1], "r");
+    // check if this isn't a nullptr
+    if (MemoryCard == NUll)
+    {
+        printf("Error While Opining the Image")
+        return 2;
+    }
     //Repeat until end of card:
         //Read 512 bytes into a buffer
 
