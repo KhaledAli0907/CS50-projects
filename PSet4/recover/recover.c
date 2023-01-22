5fd413ae6d638eb78514a3cd9701242b781abff3
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-22 20:59:30 +0000

    Sun, Jan 22, 2023, 8:59 PM +00:00

--- PSet4/recover/recover.c
+++ PSet4/recover/recover.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     typedef uint8_t BYTE;
     FILE *img = NULL;
     BYTE buffer[BlockSize];
-    char CurrentFile[8];
+    char CurrentImg[8];
     //check if the input argument is valid
     if (argc != 2)
     {
@@ -32,8 +32,14 @@ int main(int argc, char *argv[])
         //if start of a new JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            //Check if this old JPEG and close it
+            if (img != NULL)
+            {
+                fclose(img);
+            }
             //if first JPEG
-            
+            sprintf(CurrentImg, "%03i.JPEG",)
+
             //else
         }
 
