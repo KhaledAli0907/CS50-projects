31236ba8b17b00d082e61be4d593a4f379ce0c34
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-22 20:22:15 +0000

    Sun, Jan 22, 2023, 8:22 PM +00:00

--- PSet4/recover/recover.c
+++ PSet4/recover/recover.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
             }
             if (img != NULL)
             {
-                fwrite(buffer, sizeof(BYTE), BlockSize, img);
+                fwrite(buffer, sizeof(buffer), 1, img);
             }
         }
         //close the opend files
