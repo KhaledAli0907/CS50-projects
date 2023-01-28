f49dda0e47edc7a056ae62f606fca5a98cf4e95c
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-28 17:07:15 +0000

    Sat, Jan 28, 2023, 7:07 PM +02:00

--- try.c
+++ try.c
@@ -24,30 +24,24 @@ node_t CreateNewNode(int value)
     node_t *result = malloc(sizeof(node_t));
     result->value = value;
     result->next = NULL;
-    return result;
+    return *result;
 }
 
-node_t insert(node_t *head, node_t* NodeToInsert)
+node_t InsertAtHead(node_t *head, node_t* NodeToInsert)
 {
     NodeToInsert->next = head;
-    return NodeToInsert;
+    return *NodeToInsert;
 }
 
 int main()
 {
-    node_t n1, n2, n3;
+    node_t *tmp;
     node_t *head;
-
-    n1.value = 32;
-    n2.value = 16;
-    n3.value = 5;
-
-    head = &n3;
-    n3.next = &n2;
-    n2.next = &n1;
-    n1.next = NULL;
-    CreateNewNode(72);
-
+    for (int i = 0; i < 25; i++)
+    {
+        tmp = CreatNewNode(i);
+        head = InsertAtHead(head, tmp);
+    }
     printlist(head);
     return 0;
 
