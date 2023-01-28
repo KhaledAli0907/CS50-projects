e20e9b853eed26d218abc500095c0f4c33087ba6
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-28 16:31:40 +0000

    Sat, Jan 28, 2023, 6:31 PM +02:00

--- try.c
+++ try.c
@@ -8,7 +8,20 @@ struct node
 };
 typedef struct node node_t;
 
+void print
+
 int main()
 {
-    node
+    node_t n1, n2, n3;
+    ndoe_t *head;
+
+    n1.value = 32;
+    n2.value = 16;
+    n3.value = 5;
+
+    head = &n3;
+    n3.next = &n2;
+    n2.next = &n1;
+    n1.next = NULL;
+
 }
