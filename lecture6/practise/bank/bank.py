930d8b2e0335eb1457b1217502ee5c888cf83f95
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-30 17:24:44 +0000

    Mon, Jan 30, 2023, 7:24 PM +02:00

--- lecture6/practise/bank/bank.py
+++ lecture6/practise/bank/bank.py
@@ -4,7 +4,7 @@ greet = input("Greeting: ")
 greet = greet.lower()
 if greet[:2] in ["h" + "e"]:
     print("0$")
-elif greet == "how you doing?":
+elif greet[:1] == "h":
     print("20$")
 else:
     print("100$")
