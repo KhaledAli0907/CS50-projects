67624920b2cb489e55f1aa978d51fbf796de0103
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-30 17:54:04 +0000

    Mon, Jan 30, 2023, 7:54 PM +02:00

--- lecture6/practise/figlet/figlet.py
+++ lecture6/practise/figlet/figlet.py
@@ -3,17 +3,17 @@ from pyfiglet import Figlet
 import random
 
 
-if len(sys.argv) = 1:
+if len(sys.argv) == 2:
     print("please provide the name of the font")
     sys.exit(1)
 
 figlet = Figlet()
 
-if len(sys.argv) = 2:
+if len(sys.argv) == 3:
     FigList = figlet.getFonts()
     FigList.setFont(argv[1])
     print(figlet.renderText(argv[2]))
 
-if len(sys.argv) = 0:
+if len(sys.argv) == 1:
     random.Random(figlet)
 
