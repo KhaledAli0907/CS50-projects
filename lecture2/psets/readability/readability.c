eff49d82ec17ecc806c4868fa9a8ac24dab20056
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 14:47:16 +0000

    Tue, Jan 31, 2023, 4:47 PM +02:00

--- lecture2/psets/readability/readability.c
+++ lecture2/psets/readability/readability.c
@@ -5,43 +5,43 @@
 #include<math.h>
 
 int main (void)
-    {
-        string Text = get_string("Enter Your Text Here: ");
-        int Letters=0;
-        int Words=0;
-        int Sentence=0;
+{
+    string Text = get_string("Enter Your Text Here: ");
+    int Letters = 0;
+    int Words = 0;
+    int Sentence = 0;
 
-        for (int l = 0; l < strlen(Text) ;l++)
+    for (int l = 0; l < strlen(Text) ;l++)
+        {
+            if (isalpha(Text[l]))
             {
-                if(isalpha(Text[l]))
-                {
-                 Letters++;
-                }
-                else if(isspace (Text[l]))
-                {
-                 Words++;
-                }
-                else if(Text[l]=='.'||Text[l]=='?'||Text[l]=='!')
-                {
-                 Sentence++;
-                }
+                Letters++;
             }
+            else if (isspace (Text[l]))
+            {
+                Words++;
+            }
+            else if (Text[l]=='.'||Text[l]=='?'||Text[l]=='!')
+            {
+                Sentence++;
+            }
+        }
 
-        float L = (Letters*100)/(Words+1);
-        float S = (Sentence*100)/(Words+1);
-        float Grade = 0.0588 * L - 0.296 * S - 15.8;
-       //printf("Your Grade is: %f \n",Grade);
+    float L = (Letters*100)/(Words+1);
+    float S = (Sentence*100)/(Words+1);
+    float Grade = 0.0588 * L - 0.296 * S - 15.8;
+        //printf("Your Grade is: %f \n",Grade);
 
-        if((int) round(Grade)<=1)
-        {
-            printf("Before Grade 1\n");
-        }
-        else if((int) round(Grade)>1 && (int) round(Grade)<16)
-        {
-            printf("Grade %i\n",(int) round(Grade));
-        }
-        else if((int) round(Grade)>=16)
-        {
-            printf("Grade 16+\n");
-        }
+    if ((int) round(Grade)<=1)
+    {
+        printf("Before Grade 1\n");
     }
+    else if ((int) round(Grade)>1 && (int) round(Grade)<16)
+    {
+        printf("Grade %i\n",(int) round(Grade));
+    }
+    else if ((int) round(Grade)>=16)
+    {
+        printf("Grade 16+\n");
+    }
+}
