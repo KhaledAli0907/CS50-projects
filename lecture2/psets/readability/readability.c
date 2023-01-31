f1d876b7401898dabe53308fb3389de5ba1f42fe
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 14:32:24 +0000

    Tue, Jan 31, 2023, 4:32 PM +02:00

---
+++ lecture2/psets/readability/readability.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(argc, argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./readability key")
+        return
+    }
+    int words = 0;
+    int letters = 0;
+    int 
+}
