a04769a7504bc7df842a90b936aff00cbfe48135
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 15:44:32 +0000

    Tue, Jan 31, 2023, 5:44 PM +02:00

--- lecture6/pset/world-cup/tournament.py
+++ lecture6/pset/world-cup/tournament.py
@@ -19,7 +19,7 @@ def main():
     #open teams csv file
     with open(sys.argv[1], "r") as file:
         #read from the file
-        
+        csv.DictReader
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
