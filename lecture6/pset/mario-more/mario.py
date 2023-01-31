eee8deced5cfeebabff4560a674eb5f34a142f86
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 20:05:56 +0000

    Tue, Jan 31, 2023, 10:05 PM +02:00

--- lecture6/pset/mario-more/mario.py
+++ lecture6/pset/mario-more/mario.py
@@ -1,1 +1,32 @@
 # TODO
+def main():
+    # Set the height value
+    height = get_height()
+    # loop to go through rows
+    for row in range(height):
+        # loop to print spaces
+        for space in range(height, row+1, -1):
+            print(" ", end="")
+        # loop to print hashes
+        for hash in range(0, row+1, +1):
+            print("#", end="")
+        print()
+
+
+
+
+# Fuction to ask for valid user input
+def get_height():
+    while True:
+        try:
+            # Get input from the user
+            n = int(input("Enter Height: "))
+            # make user it's between 1 and 8
+            if n >= 1 and n <= 8:
+                return n
+        except ValueError:
+            print("Invalid Number")
+
+
+if __name__ == "__main__":
+    main()
