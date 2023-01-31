34a3e8f0c1d27d88cd58ed79c25dee2084f102e1
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 21:08:54 +0000

    Tue, Jan 31, 2023, 11:08 PM +02:00

--- lecture6/pset/sentimental-readability/readability.py
+++ lecture6/pset/sentimental-readability/readability.py
@@ -2,8 +2,15 @@
 from cs50 import get_string()
 
 def main():
+    # get the input from user
     text = get_string("Enter Text Here: ")
-    
+    # declaring variables
+    letters = 0
+    words = 0
+    sentence = 0
+    # loop to go through text
+    for i in range(strlen)
+
 
 
 
