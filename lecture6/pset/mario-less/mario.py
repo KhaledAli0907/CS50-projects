7dbe954355ac3d5e3ca661c5390085c3d6275e22
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 19:33:02 +0000

    Tue, Jan 31, 2023, 9:33 PM +02:00

--- lecture6/pset/mario-less/mario.py
+++ lecture6/pset/mario-less/mario.py
@@ -12,12 +12,15 @@ def main():
 #function to get hight
 def get_height():
     while True:
-        #get the user input while its between 1 and 8
-        n = get_int("Height: ")
-        if n > 0 or n <= 8:
-            return n
-        else:
-            print("not between 1 and 8")
+        try:
+            #get the user input while its between 1 and 8
+            n = int(input("Height: "))
+            if n > 0 or n <= 8:
+                return n
+            except ValueError:
+                print("not between 1 and 8")
+
+
 
 main()
 
