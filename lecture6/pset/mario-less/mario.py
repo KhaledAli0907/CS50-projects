afde1801575278bca3d81055f103c229e0fb77d9
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 19:49:36 +0000

    Tue, Jan 31, 2023, 9:49 PM +02:00

--- lecture6/pset/mario-less/mario.py
+++ lecture6/pset/mario-less/mario.py
@@ -1,28 +1,27 @@
 def main():
-    #store height
+    # store height
     height = get_height()
-    #loop to through rows
+    # loop to through rows
     for row in range(height):
-        #loop to print spaces
+        # loop to print spaces
         for space in range(height, row+1, -1):
             print(" ", end="")
-        #loop to print hashes
+        # loop to print hashes
         for hash in range(0, row+1, 1):
             print("#", end="")
         print()
 
 
-#function to get hight
+# function to get hight
 def get_height():
     while True:
         try:
-            #get the user input while its between 1 and 8
+            # get the user input while its between 1 and 8
             n = int(input("Height: "))
             if n > 0 and n <= 8:
                 return n
         except ValueError:
             print("not between 1 and 8")
-
 
 
 main()
