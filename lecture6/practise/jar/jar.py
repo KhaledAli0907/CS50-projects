8286fa72b07b556ec768fd7307f2b6428bc2ee3d
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 12:17:24 +0000

    Tue, Jan 31, 2023, 2:17 PM +02:00

--- lecture6/practise/jar/jar.py
+++ lecture6/practise/jar/jar.py
@@ -1,35 +1,36 @@
-
+def main():
+    jar = Jar()
+    print(str(jar.capacity))
 
 class Jar:
     def __init__(self, capacity=12):
-        if self.size < 0:
+        if size < 0:
             raise ValueError("Only Positive integers allowed")
 
 
     def __str__(self):
-        n = self.size
+        n = size
         return n
 
     def deposit(self, n):
         for n in range(self.capacity):
-            self.size += n
+            size += n
 
     def withdraw(self, n):
         if n in range(self.size):
-            self.size -= n
+            size -= n
         else:
             raise ValueError("Not enough cookies in the jar")
 
     @property
     def capacity(self):
-        return self.capacity
+        capacity = self.capacity
+        return capacity
 
     @property
     def size(self):
-        return self.size
+        size = self.size
+        return size
 
-def main():
-    jar = Jar()
-    print(str(jar.capacity))
-    jar.deposit(2)
-    print(str(jar))
+main()
+
