f5cee7e02ab58dd28a8f7a408519d0be9fc91ca8
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 19:00:39 +0000

    Tue, Jan 31, 2023, 9:00 PM +02:00

--- lecture6/pset/mario-less/mario.py
+++ lecture6/pset/mario-less/mario.py
@@ -1,9 +1,13 @@
 from cs50 import get_int()
-
+height = get_height()
 
 
 #function to get hight
 def get_height():
     while True:
-        
+        n = get_int()
+        if n > 0 or n <= 8:
+            return n
+
+
 
