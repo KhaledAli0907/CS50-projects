60a01d8037fce7faee1e88012fed5c5138d67966
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 10:13:06 +0000

    Tue, Jan 31, 2023, 12:13 PM +02:00

--- lecture6/practise/jar/jar.py
+++ lecture6/practise/jar/jar.py
@@ -1,11 +1,11 @@
 class Jar:
     def __init__(self, capacity=12):
         if capacity < 0:
-            raise ValueError ()
+            raise ValueError("Only Positive Characters are allowed")
 
 
     def __str__(self):
-        ...
+        
 
     def deposit(self, n):
         ...
