c172753a2eab5cb5e9daa304bd07d33cb388ed6b
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 14:41:06 +0000

    Tue, Jan 31, 2023, 4:41 PM +02:00

--- lecture2/psets/readability/readability.c
+++ lecture2/psets/readability/readability.c
@@ -1,22 +1,48 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<cs50.h>
+#include<ctype.h>
+#include<string.h>
+#include<math.h>
 
-int main(argc, argv[])
-{
-    if (argc != 2)
+int main (void)
     {
-        printf("Usage: ./readability key")
-        return
-    }
-    string Text = get_string("Text: ");
-    int words = 0;
-    int letters = 0;
-    int sentence = 0;
+        string Text = get_string("Enter Your Text Here: ");
+        int Letters=0;
+        int Words=0;
+        int Sentence=0;
 
-    for (int i = 0; i < Text; i++)
-    {
-        if (isalpha(argv))
+        for (int l=0;l<strlen(Text);l++)
+            {
+                if(isalpha(Text[l]))
+                {
+                 Letters++;
+                }
+                else if(isspace (Text[l]))
+                {
+                 Words++;
+                }
+                else if(Text[l]=='.'||Text[l]=='?'||Text[l]=='!')
+                {
+                 Sentence++;
+                }
+            }
+
+        float L = (Letters*100)/(Words+1);
+        float S = (Sentence*100)/(Words+1);
+        float Grade = 0.0588 * L - 0.296 * S - 15.8;
+       //printf("Your Grade is: %f \n",Grade);
+
+        if((int) round(Grade)<=1)
+        {
+            printf("Before Grade 1 \n");
+        }
+        else if((int) round(Grade)>1 && (int) round(Grade)<16)
+        {
+            printf("Grade %i \n",(int) round(Grade));
+        }
+        else if((int) round(Grade)>=16)
+        {
+            printf("Grade 16+ \n");
+        }
     }
-}
+    
