570a146655da7dfae756abba6854fe76a94a02c9
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 19:06:04 +0000

    Tue, Jan 31, 2023, 9:06 PM +02:00

--- lecture6/pset/mario-less/mario.py
+++ lecture6/pset/mario-less/mario.py
@@ -1,13 +1,18 @@
 from cs50 import get_int
-height = get_height()
+def main():
+    height = get_height()
+    for i in range (height):
+        print ("#")
+
 
 
 #function to get hight
 def get_height():
     while True:
+        #get the user input while its between 1 and 8
         n = get_int()
         if n > 0 or n <= 8:
             return n
 
-
+main()
 
