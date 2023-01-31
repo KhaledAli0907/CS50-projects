3795dd2a8b25dbb1809b2b79a89b1d2dc3e300e1
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 18:15:15 +0000

    Tue, Jan 31, 2023, 8:15 PM +02:00

--- lecture1/psets/mario-less/mario.c
+++ lecture1/psets/mario-less/mario.c
@@ -3,7 +3,12 @@
 
 int main(void)
 {
-    int height = get_int("Height: ");
+    int height;
+    do
+    {
+        height = get_int("enter Positive Number: ")
+    }
+    while (height)
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < height; j++)
