06c7bd3beb7bb9189b3ccf629050059adeb33e29
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-01-31 20:30:10 +0000

    Tue, Jan 31, 2023, 10:30 PM +02:00

--- lecture1/psets/mario-more/mario.c
+++ lecture1/psets/mario-more/mario.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <cs50.h>
 
-do
+int main(void)
 {
-    int
+    int height;
+    // get user input while making sure it's between 1 and 8
+    do
+    {
+        height = get_int("Enter Height:");
+    }
+    while(height < 1 || height > 8);
+    // loop through raws
+    for (int row = 0; row < height; row++)
+    {
+        // loop and print spaces
+        for (int space = height; space >= row+1; space--)
+        {
+            printf(" ");
+        }
+        // loop and print hashes
+        for (int hash = 0; hash <= row+1; hash++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
