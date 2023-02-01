8d7fef9056904cf4fccecf73f7af5d29c7f1c6e9
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 11:00:52 +0000

    Wed, Feb 1, 2023, 1:00 PM +02:00

--- lecture6/pset/sentimental-cash/cash.py
+++ lecture6/pset/sentimental-cash/cash.py
@@ -1,7 +1,10 @@
 # TODO
 def main():
+    # ask how many cents the customer is owed
     cents = int(input("Change Owed: "))
-
+    # Calculate the quarters
+    quarters = (cents / 25) - (quarters * 25)
+    # Calculate Dimes
 
 if __name__ == "__main__":
     main()
