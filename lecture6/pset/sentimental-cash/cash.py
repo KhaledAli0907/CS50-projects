6706d8267896dd6660978882d7d90a66a7146dfc
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 11:07:24 +0000

    Wed, Feb 1, 2023, 1:07 PM +02:00

--- lecture6/pset/sentimental-cash/cash.py
+++ lecture6/pset/sentimental-cash/cash.py
@@ -1,14 +1,21 @@
 # TODO
 def main():
     # ask how many cents the customer is owed
-    cents = int(input("Change Owed: "))
+    cents = GetCents()
     # Calculate the quarters
     quarters = (cents / 25) - (quarters * 25)
     # Calculate Dimes
 
 
-def GetInput():
-    
+def GetCents():
+    while True:
+        try:
+            cents = int(input("Change owed: "))
+            if cents > 0:
+                return cents
+        except ValueError:
+            print("Invaild value")
+
 
 if __name__ == "__main__":
     main()
