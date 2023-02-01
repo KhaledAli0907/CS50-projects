e974c65da684d3e7ba6311403f840727e110f8f4
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 13:36:37 +0000

    Wed, Feb 1, 2023, 3:36 PM +02:00

--- lecture6/pset/sentimental-credit/credit.py
+++ lecture6/pset/sentimental-credit/credit.py
@@ -14,35 +14,7 @@ def UserInput():
             print("Invalid Input")
 
 def ValidateCard(CreditCard):
-    # store the length of CridetCard Num.
-    x = len(CreditCard)
-    # check if the length is valid
-    if x < 13 or x > 16:
-        print("Inavild Card number")
-        sys.exit(0)
-
-    even, odd = 0, 0
-    # if the Card length is even
-    if x % 2 == 0:
-        # loop through card lengths
-        for i in range(x):
-            num = int(CreditCard[i])
-            # if the spot we looping over is even
-            if i % 2 == 0:
-                multiply = num * 2
-                if multiply >= 10:
-                    even += multiply // 10
-                    even += multiply % 10
-                else:
-                    even += multiply
-            else:
-                odd += num
-    # if the card length is odd
-    else:
-        for j in range(x):
-
-
-
+    
 
 
 if __name__ == "__main__":
