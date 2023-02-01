9ad166797f265390c458cc32644cb24e7da934d2
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 11:59:57 +0000

    Wed, Feb 1, 2023, 1:59 PM +02:00

--- lecture1/psets/credit/credit.c
+++ lecture1/psets/credit/credit.c
@@ -6,6 +6,9 @@ int main(void)
     // promt the user for the card number
     int x = get_int("Credit card number: ");
     // do the check sum
-    
+    // Store the last number in the card into variable
+    int y = x % 10;
+    //store the las two number 
+
 
 }
