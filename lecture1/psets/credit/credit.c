69aef1058ddcf5629207f621ed84d99cb880ce07
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 11:57:40 +0000

    Wed, Feb 1, 2023, 1:57 PM +02:00

--- lecture1/psets/credit/credit.c
+++ lecture1/psets/credit/credit.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    // promt the user for the card number
+    int x = get_int("Credit card number: ");
+    // do the check sum
     
+
 }
