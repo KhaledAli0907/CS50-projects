b201c563999cd8d8b5411b53f93addd7da4ad5ea
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 14:06:02 +0000

    Wed, Feb 1, 2023, 4:06 PM +02:00

--- lecture6/pset/sentimental-credit/credit.py
+++ lecture6/pset/sentimental-credit/credit.py
@@ -12,14 +12,22 @@ def UserInput():
             print("Invalid Input")
 
 def CheckCard(CreditCard):
+    # Hold the Card num. list
     y = len(CreditCard)
+    # Copy the Card into list to modify
     copycard = CreditCard[]
+    # apply the algorithm
     firstnumbers = [int(i) for i in range(copycard[0::2])]
     othernumbers = [(int(x) * 2 // 10) + (int(x) * 2 % 10) for x in range(copycard[1::0])]
     totalsum = sum(firstnumbers) + sum(othernumbers)
+    #check the type of Card if it's valid
     if totalsum % 10 == 0:
         if y in [13, 16] and CreditCard[0] == 4:
-            
+            print("VISA")
+        elif y == 15 and CreditCard[0:2] in [34, 37]:
+            print("AMEX")
+        elif y == 16 and CreditCard[0]
+
 
 
 
