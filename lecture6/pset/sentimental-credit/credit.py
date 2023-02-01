37995ae07c8efed9a6d0c3d6221b383da05afa18
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 12:18:56 +0000

    Wed, Feb 1, 2023, 2:18 PM +02:00

---
+++ lecture6/pset/sentimental-credit/credit.py
@@ -0,0 +1,15 @@
+# TODO
+def main():
+
+
+def UserInput():
+    while True:
+        try:
+            CNum = long(input("CridetCard Number: "))
+            
+
+
+
+
+if __name__ == "__main__":
+    main()
