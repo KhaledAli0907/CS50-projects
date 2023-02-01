4725b02a2c9f61edfc543b28638ec1e6e19926dd
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-01 12:30:37 +0000

    Wed, Feb 1, 2023, 2:30 PM +02:00

--- lecture6/pset/sentimental-credit/credit.py
+++ lecture6/pset/sentimental-credit/credit.py
@@ -1,13 +1,15 @@
 # TODO
 def main():
-
+    x = UserInput()
 
 def UserInput():
     while True:
         try:
             CNum = long(input("CridetCard Number: "))
-            
-
+            if CNum > 0:
+                return CNum
+        except ValueError:
+            print("Invalid Input")
 
 
 
