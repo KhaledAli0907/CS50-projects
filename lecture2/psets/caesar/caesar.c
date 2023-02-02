5777b51601b963ec5a8a74852d902d3344e53c08
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-02 14:47:59 +0000

    Thu, Feb 2, 2023, 4:47 PM +02:00

--- lecture2/psets/caesar/caesar.c
+++ lecture2/psets/caesar/caesar.c
@@ -1,19 +1,54 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 
 int main(int argc, string argv[])
 {
+    // make the sure the user entered the right command
     if (argc != 2)
     {
         printf("Usage: ./caesar key");
         return 1;
     }
-    if (!isalpha(argv[1]))
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        printf("Usage: ./carsar key");
-        return 1;
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./carsar key");
+            return 1;
+        }
     }
-    
+    // store the key as an integer
+    int key = atoi(argv[1]);
+    // ask the user for text
+    string Text = get_string("Enter Text Here: ");
+    // return the cyphered text
+    printf("Cypherd Text: ");
+    // encryption loop
+    for (int i = 0; i < strlen(Text); i++)
+    {
+        // check if its alphabetical
+        if (isalpha(Text[i]))
+        {
+            // check if it's UpperCase
+            if (isupper(Text[i]))
+            {
+                int cupper = (((Text[i] - 65) + key) % 26) + 65;
+                printf("%c", cupper);
+            }
+            // check if it's lowerCase
+            else if (islower(Text[i]))
+            {
+                int clower = (((Text[i] - 97) + key) % 26) + 97;
+                printf("%c", clower);
+            }
+        }
+        else
+        {
+            printf("%c", Text[i])
+        }
+    }
+    printf("\n");
 }
