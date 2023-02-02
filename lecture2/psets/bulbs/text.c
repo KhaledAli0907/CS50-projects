6da707260b574e08ded83cc9808a5e4fa27a5407
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-02 22:02:33 +0000

    Fri, Feb 3, 2023, 12:02 AM +02:00

--- lecture2/psets/bulbs/text.c
+++ lecture2/psets/bulbs/text.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(void)
 {
     string text = get_string("text: ");
     for (int i = 0; i < strlen(text);i++)
     {
-        int m = atoi(text[i]);
+        int m = toascii(text[i]);
         printf("number: %i /n", m);
     }
 
