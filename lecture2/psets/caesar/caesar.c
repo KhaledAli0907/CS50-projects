abe3b31ec7cff1df128ae1ac28dc473c60d0b24d
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-02 14:30:18 +0000

    Thu, Feb 2, 2023, 4:30 PM +02:00

--- lecture2/psets/caesar/caesar.c
+++ lecture2/psets/caesar/caesar.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+
 
 int main(int argc, string argv[])
 {
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+    if (!isalpha(argv[1]))
+    {
+        printf("Usage: ./carsar key");
+        return 1;
+    }
     
 }
