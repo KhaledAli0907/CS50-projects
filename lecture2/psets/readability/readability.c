64bb6e2aaf2ca4fb1cfed1f84a7c198ea198dc34
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-02 17:48:19 +0000

    Thu, Feb 2, 2023, 7:48 PM +02:00

--- lecture2/psets/readability/readability.c
+++ lecture2/psets/readability/readability.c
@@ -36,7 +36,11 @@ int main (void)
     {
         printf("Before Grade 1\n");
     }
-    else if ((int) round(Grade) > 1 && (int) round(Grade) < 16)
+    else if ((int) round(Grade) > 1 && (int) round(Grade) < 7.6)
+    {
+        printf("Grade %i \n", (int) round(Grade);)
+    }
+    else if ((int) round(Grade) > 7.7 && (int) round(Grade) < 16)
     {
         printf("Grade %i\n",(int) round(Grade));
     }
