d83c69f76cc4de28f2c3a6ffac117cc82db6eec7
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-02 20:54:27 +0000

    Thu, Feb 2, 2023, 10:54 PM +02:00

---
+++ lecture2/psets/substitution/substitution.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    // store the key in string
+    string key = argv[1];
+    // check the command line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key");
+        return 1;
+    }
+    // validate it's a 26 alpha chars.
+    for (int i = 0; i < strlen(key); i++)
+    {
+        
+    }
+}
