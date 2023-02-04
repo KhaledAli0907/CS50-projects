2aaa4acb0f74bbada8beead51d003497ceef8cee
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-04 21:32:42 +0000

    Sat, Feb 4, 2023, 11:32 PM +02:00

--- lecture7/lab/songs/6.sql
+++ lecture7/lab/songs/6.sql
@@ -1,1 +1,2 @@
-SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists Where name = 'Post Malone');
+--SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists Where name = 'Post Malone');
+SELECT * FROM songs JOIN artist_id = artists.id WHERE
