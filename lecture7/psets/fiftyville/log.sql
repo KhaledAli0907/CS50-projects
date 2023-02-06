64a9f7a964dde022460c1702a9fd1382cfb171ac
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 21:00:17 +0000

    Mon, Feb 6, 2023, 11:00 PM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -19,12 +19,12 @@ AND day = 28 AND hour = 10
 AND minute BETWEEN 15 and 25 AND activity = 'exit';
 
 -- See the atm trancations
-SELECT name FROM people
+SELECT people.name FROM people
 JOIN bank_accounts ON people.id = bank_accounts.person_id
 JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
 WHERE year = 2021 AND month = 7
 AND day = 28
-AND 
+AND atm_location = 'Leggett Street'
 AND transaction_type = 'withdraw';
 
 -- See the phone calls less than minute
