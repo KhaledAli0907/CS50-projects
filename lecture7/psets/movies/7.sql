99b88d252b39aeb30bf7b171a05dd73906f48569
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 17:20:03 +0000

    Mon, Feb 6, 2023, 7:20 PM +02:00

--- lecture7/psets/movies/7.sql
+++ lecture7/psets/movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 ORDER BY rating DESC WHERE ra;
+SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 ORDER BY rating DESC WHERE rating = rating ORDER BY title;
