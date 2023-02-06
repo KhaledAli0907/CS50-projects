4ef044456cdb0ee727d2cf0f71dcf24b05955193
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 18:28:28 +0000

    Mon, Feb 6, 2023, 8:28 PM +02:00

--- lecture7/psets/movies/12.sql
+++ lecture7/psets/movies/12.sql
@@ -2,5 +2,8 @@ SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE people.name = 'Johnny Depp'
-AND people.name = 'Helena Bonham Carter'
+AND title IN (SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE people.name = 'Helena Bonham Carter')
 ORDER BY movies.year;
