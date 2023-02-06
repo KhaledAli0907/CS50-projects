275d81840a317848f86bf0e979e3f2da2d5a8025
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 20:58:12 +0000

    Mon, Feb 6, 2023, 10:58 PM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -24,6 +24,7 @@ JOIN bank_accounts ON people.id = bank_accounts.person_id
 JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
 WHERE year = 2021 AND month = 7
 AND day = 28
+AND 
 AND transaction_type = 'withdraw';
 
 -- See the phone calls less than minute
