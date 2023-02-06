a9c9af48e52137bbc47b7a5d51227c6750ffba87
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 20:04:20 +0000

    Mon, Feb 6, 2023, 10:04 PM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -18,6 +18,6 @@ WHERE year = 2021 AND month = 7
 AND day = 28;
 
 -- See the phone calls less than minute
-SELECT caller, receiver, duration FROM phone_calls
+SELECT * FROM phone_calls
 WHERE year = 2021 AND month = 7
-AND day = 28 AND duration = 100;
+AND day = 28 AND duration < 60;
