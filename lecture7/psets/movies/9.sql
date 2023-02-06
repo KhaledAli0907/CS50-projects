e9828867303bfb3e173935f7361ba459dc9563a3
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 17:57:33 +0000

    Mon, Feb 6, 2023, 7:57 PM +02:00

--- lecture7/psets/movies/9.sql
+++ lecture7/psets/movies/9.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
+JOIN stars ON stars.people_id = people.id
+JOIN movies ON movies.id = stars.movie_id
 WHERE movies.year = 2004 ORDER BY people.birth;
