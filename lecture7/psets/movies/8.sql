f61799b9c2eb8c34b043e4e7391cf65bf5ec9654
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 17:30:39 +0000

    Mon, Feb 6, 2023, 7:30 PM +02:00

--- lecture7/psets/movies/8.sql
+++ lecture7/psets/movies/8.sql
@@ -1,1 +1,4 @@
-SELECT people.name FROM people join stars ON people.id = start.person_id
+SELECT people.name FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE title = 'Toy Story';
