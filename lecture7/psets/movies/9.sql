ad6605f35dec48b1ad608b8d05778bc73c9ecf13
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 17:37:43 +0000

    Mon, Feb 6, 2023, 7:37 PM +02:00

--- lecture7/psets/movies/9.sql
+++ lecture7/psets/movies/9.sql
@@ -1,4 +1,4 @@
 SELECT people.name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-WHERE year = 2004 ORDER BY people.birth;
+WHERE year = 2004 AND  ORDER BY people.birth;
