30d7d23a3b734b61bc065b86cc1d69b316ff6342
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 19:08:49 +0000

    Mon, Feb 6, 2023, 9:08 PM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -1,2 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+-- quarry to se the descrption of the Crime
+SELECT description FROM crime_scene_reports
+WHERE year = 2021 AND month = 7
+AND day = 28
+AND street = 'Humphrey Street';
+
 
