a08723fc9cf6d82767fa40dc5288b2d7b5eaefa1
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 19:32:34 +0000

    Mon, Feb 6, 2023, 9:32 PM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -15,4 +15,4 @@ SELECT name FROM people
 JOIN bank_accounts ON people.id = bank_accounts.person_id
 JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
 WHERE year = 2021 AND month = 7
-AND day = 28 AND atm_transaction.transaction_tybe = 'withdraw';
+AND day = 28;
