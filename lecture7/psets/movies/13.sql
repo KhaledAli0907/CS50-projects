6b6e8f8fed094a9dd57e7b1a82a9df5877c81874
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-06 18:38:00 +0000

    Mon, Feb 6, 2023, 8:38 PM +02:00

--- lecture7/psets/movies/13.sql
+++ lecture7/psets/movies/13.sql
@@ -2,4 +2,8 @@ SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.id IN
-()
+(SELECT movies.id FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE name = 'Kevin Bacon')
+AND 
