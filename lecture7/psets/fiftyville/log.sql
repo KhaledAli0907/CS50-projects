aaeb998f622ae7d9fc3b064aa50dc69802e131cb
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-07 09:34:57 +0000

    Tue, Feb 7, 2023, 11:34 AM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -47,7 +47,7 @@ WHERE year = 2021 AND month = 7
 AND day = 28 AND duration < 60;
 
 -- insert into susbects list
-INSERT INTO susbects (name) VALUES (bruce, diana, iman, luca);
+INSERT INTO susbects VALUES (bruce, diana, iman, luca);
 
 -- See the name of the FarmVille airport
 SELECT abbreviation, full_name, city FROM airports;
@@ -67,7 +67,7 @@ day = 29 AND hour = 8 AND minute = 20
 ORDER BY people.name;
 
 -- Update the susbicts list
-INSERT INTO susbects (name) VALUES (bruce, luca);
+DELETE FROM susbects WHERE name = 'iman' AND name = 'Diana';
 
 
 -- bruce, diana, iman and luca are suspects
