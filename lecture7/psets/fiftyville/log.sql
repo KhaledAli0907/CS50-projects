bf655bbe00c68ec1ac8a9f9b8a3be740f0a998a1
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-07 07:59:30 +0000

    Tue, Feb 7, 2023, 9:59 AM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -20,7 +20,7 @@ AND day = 28 AND hour = 10
 AND minute BETWEEN 15 and 25 AND activity = 'exit';
 
 -- See the atm trancations
-SELECT people.name, atm_transactions.hour, atm_transactions.minute
+SELECT people.name, atm_transactions.amount
 FROM people
 JOIN bank_accounts ON people.id = bank_accounts.person_id
 JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
