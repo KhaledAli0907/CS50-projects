22266e063e70aad607c43ad22e88038a48c9f451
KhaledAli0907 <113885383+KhaledAli0907@users.noreply.github.com>
2023-02-07 07:55:56 +0000

    Tue, Feb 7, 2023, 9:55 AM +02:00

--- lecture7/psets/fiftyville/log.sql
+++ lecture7/psets/fiftyville/log.sql
@@ -12,7 +12,8 @@ AND day = 28 AND transcript LIKE '%bakery%';
 
 -- See the bakery security logs
 SELECT people.name, bakery_security_logs.license_plate,
-bakery_security_logs.activity FROM people
+bakery_security_logs.activity, bakery_security_logs.hour, bakery_security_logs.minute
+FROM people
 JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
 WHERE year = 2021 AND month = 7
 AND day = 28 AND hour = 10
